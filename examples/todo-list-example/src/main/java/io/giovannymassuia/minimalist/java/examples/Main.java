ba77cb45359f59518f901959a4aceaf4ba31d52b
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-10-29 20:54:19 -0700

    🎉 feat: todo-list example init

--- examples/todo-list-example/src/main/java/io/giovannymassuia/minimalist/java/examples/Main.java
+++ examples/todo-list-example/src/main/java/io/giovannymassuia/minimalist/java/examples/Main.java
@@ -1,7 +1,16 @@
 package io.giovannymassuia.minimalist.java.examples;
 
+import io.giovannymassuia.minimalist.java.Api;
+import io.giovannymassuia.minimalist.java.DependencyInjection;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Api api = new Api();
+        DependencyInjection di = new DependencyInjection();
+
+        api.hello();
+        di.hello();
     }
 }
